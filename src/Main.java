84eb56bf97f153124b403c44637fa4f37361800d
eremi <ereminr83@mail.ru>
2023-03-02 21:19:44 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        System.out.println();
+
+
+    }
+
+    public static void task1() {
+        System.out.println("1");
+
+    }
+
+}
